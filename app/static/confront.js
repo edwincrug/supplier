eb11ce2e7cd100cac400f0bdf65674db1a71fbb8
edwincrug <edwincrug@gmail.com>
2016-03-07 18:23:24 -0600

    Componentes diseño

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,4 +1,4 @@
 var global_settings = {
-	urlCORS: 'http://localhost:55442',
+	urlCORS: 'http://192.168.20.9/ProveedorApi',
 	liveReload: 1000
 } 	
--- app/static/min/confront-min.js
+++ app/static/min/confront-min.js
@@ -1,1 +1,1 @@
-var global_settings={urlCORS:"http://192.168.20.9/ApiNotify",liveReload:1e3};
+var global_settings={urlCORS:"http://192.168.20.9/ProveedorApi",liveReload:1e3};
