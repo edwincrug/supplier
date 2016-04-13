68494627f44da5f9e860be1207ad11d9042b36b3
masc0202 <masc_0202@hotmail.com>
2016-04-13 18:46:15 -0500

    todo
    todo

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,4 +1,6 @@
 var global_settings = {
-	urlCORS: 'http://192.168.20.9/ProveedorApi',
+	//urlCORS: 'http://192.168.20.9/ProveedorApi',
+	//urlCORS: 'http://localhost:55442'
+	  urlCORS: 'http://localhost:55442',
 	liveReload: 1000
 } 	
